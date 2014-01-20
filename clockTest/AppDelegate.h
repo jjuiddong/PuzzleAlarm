492f7e020eb06cdbdbd71434e5c14be95377eed6
jjuiddong <jjuiddong@hanmail.net>
2014-01-20 17:42:41 +0900

    first commit

---
+++ clockTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  clockTest
+//
+//  Created by 재정 이 on 14. 1. 20..
+//  Copyright (c) 2014년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ clockTest/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  clockTest
+//
+//  Created by 재정 이 on 14. 1. 20..
+//  Copyright (c) 2014년 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
